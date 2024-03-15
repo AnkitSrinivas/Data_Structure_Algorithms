b4d10b0def7e88234c3d07195b2509a5fa36f349
AnkitSrinivas <84558594+AnkitSrinivas@users.noreply.github.com>
2024-03-15 21:24:04 +0530

    Update TwoSumArray.js

--- Arrays/TwoSumArray.js
+++ Arrays/TwoSumArray.js
@@ -31,24 +31,6 @@ const twoSumOptimal = (arr,target) => {
 }
 
 
-// Optimal solution with time complexity O{n} and space complexity O(n)
-const twoSumArray = function(arr,target){
-    let low=0,high=arr.length
-
-    while(low<high){
-        if(arr[low]+arr[high]==target){
-            return [low,high]
-        }else if((arr[low]+arr[high])>target){
-            high--
-        }
-        else{
-            low++
-        }
-    }
-
-    return -1
-}
-
 console.log(twoSum(myArr,target))
 console.log(twoSumOptimal(myArr,target))
 console.log(twoSumArray(myArr,target))
